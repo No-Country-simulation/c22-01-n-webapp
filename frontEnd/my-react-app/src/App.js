c3bc3052745200bd6f746909a4846cb187c56adb
Juan Carlos Díaz <169100845+Nxusbets@users.noreply.github.com>
2024-11-24 22:11:17 +0000

    agendar cita, registro, iniciar sesion

--- frontEnd/my-react-app/src/App.js
+++ frontEnd/my-react-app/src/App.js
@@ -1,40 +1,38 @@
-import React, { useState } from "react";
-import NavBar from "./components/navbar"; 
-import Jumbotron from "./components/jumbotron";
-import RegistrationForm from "./components/RegistrationForm"; 
-import Carrusel from "./components/Carrusel";
-import DoctorCard from "./components/DoctorCard";
-import Footer from "./components/footer";
-import Login from "./components/Login";
-import AgendarCita from "./components/AgendarCita";
+// App.js
+import React, { useState } from 'react';
+import { Routes, Route } from 'react-router-dom';
+import NavBar from './components/navbar';
+import Jumbotron from './components/jumbotron';
+import RegistrationForm from './components/RegistrationForm';
+import Carrusel from './components/Carrusel';
+import DoctorCard from './components/DoctorCard';
+import Footer from './components/footer';
+import Login from './components/Login';  // Asegúrate de importar el componente Login
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Routes, Route } from "react-router-dom";  // Importa Routes y Route
+import AgendarCita from './components/AgendarCita';
 
 const App = () => {
-  const [showForm, setShowForm] = useState(false); 
+  const [showForm, setShowForm] = useState(false);
 
   const handleRegisterClick = () => {
-    setShowForm(true); 
+    setShowForm(true);
   };
 
   return (
     <div>
-      {/* Ya no necesitas el Router aquí */}
       <NavBar onRegisterClick={handleRegisterClick} />
       <Routes>
-        {/* Define las rutas para las diferentes vistas */}
-        <Route path="/" element={<>
-          <Jumbotron />
-          <DoctorCard />
-          <Carrusel />
-          <Footer />
-        </>} />
-
-        <Route path="/login" element={<Login />} />  {/* Ruta para login */}
-        <Route path="/agendar-cita" element={<AgendarCita />} />  {/* Ruta para agendar cita */}
-        
-        {/* Condición para mostrar el formulario de registro */}
-        <Route path="/register" element={showForm ? <RegistrationForm /> : <></>} /> 
+        <Route path="/" element={
+          <>
+            <Jumbotron />
+            <DoctorCard />
+            <Carrusel />
+            <Footer />
+          </>
+        } />
+        <Route path="/login" element={<Login />} /> {/* Ruta de login */}
+        <Route path="/register" element={<RegistrationForm />} />
+        <Route path="/agendar-cita" element={<AgendarCita />} />
       </Routes>
     </div>
   );
